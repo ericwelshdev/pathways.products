11de8ddef91e84669ad2d9a6a87042be337b7d6c
ericwelshdev <ericwelsh.dev@gmail.com>
2017-01-31 13:06:18 -0800

    added new section added new support for moble ipad  and smaller laptop screen support added scroll up changed main image

--- js/index.js
+++ js/index.js
@@ -20,4 +20,38 @@ $(window).load(function () {
         height: '100%'
     });
 
+
+
+
+});
+
+$('a[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        $('#scrollUp').addClass('hideme');
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+           
+        }
+    }
+    
+});
+
+$(window).scroll(function () {
+
+    if ($('#main').is(':visible')) {
+        $('#scrollUp').addClass('hideme');
+    }
+
+    if ($('#page-content').is(':visible')) {
+        $('#scrollUp').removeClass('hideme');
+    } else {
+        $('#scrollUp').addClass('hideme');
+    }
+
+
 });
